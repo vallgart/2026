2024d2bfd26ab8a69401fe116a450c5a9bb153e7
Dmitry <70018205+vallgart@users.noreply.github.com>
2025-11-19 12:37:46 +0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -8,3 +8,53 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   });
 });
+
+
+
+const burgerBtn = document.querySelector(".toggle-mobile");
+const mobileMenu = document.querySelector(".mobile-menu");
+
+// Открываем меню
+burgerBtn.addEventListener("click", () => {
+    mobileMenu.classList.toggle("active");
+});
+
+// Закрываем меню по клику на ссылку
+document.querySelectorAll(".nav-mobile a").forEach(link => {
+    link.addEventListener("click", () => {
+        mobileMenu.classList.remove("active");
+    });
+});
+
+
+
+
+document.querySelectorAll(".rules-open").forEach(btn => {
+    btn.addEventListener("click", (e) => {
+        e.preventDefault();
+        openPopup();
+    });
+});
+
+
+const popup = document.getElementById("popup");
+const popupClose = document.getElementById("popupClose");
+
+// открыть окно
+function openPopup() {
+    popup.classList.add("active");
+    document.body.style.overflow = "hidden"; // блокируем скролл
+}
+
+// закрыть окно
+function closePopup() {
+    popup.classList.remove("active");
+    document.body.style.overflow = ""; 
+}
+
+popupClose.addEventListener("click", closePopup);
+
+// если нужно закрывать по клику вне окна:
+popup.addEventListener("click", (e) => {
+    if (e.target === popup) closePopup();
+});
